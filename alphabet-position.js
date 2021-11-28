19efc63083c71f589771adec9652dba8704e45bc
Mohammad Rahman <mbrahman@gmail.com>
2021-11-28 07:59:06 -0500

    completed alphabet-position

--- alphabet-position.js
+++ alphabet-position.js
@@ -1,5 +1,18 @@
 // Replace With Alphabet Position
 
 function alphabetPosition(text) {
-    return text
+
+    // create an array of ASCII letter a - z
+    const alphabet = new Array(26)
+
+    for (i = 1; i <= 26; i++) {
+        let char = String.fromCharCode(i + 96)
+        alphabet[i] = char
+    }
+
+    let textArr = text.toLowerCase().split('').filter(element => alphabet.includes(element))
+
+    return textArr.map(element => alphabet.indexOf(element)).join(' ')
 }
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."))
