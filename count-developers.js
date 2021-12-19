a18a2d2298b83b56ff6a2c108c52f4fbdbbb4ea4
Mohammad Rahman <mbrahman@gmail.com>
2021-12-19 07:44:48 -0500

    finished counting developers challenge

---
+++ count-developers.js
@@ -0,0 +1,19 @@
+var list1 = [
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+
+function countDevelopers(list) {
+    let count = 0;
+
+    list.forEach((el) => {
+        if (el.continent === 'Europe' && el.language === 'JavaScript') {
+            count++
+        }
+    })
+    return count
+}
+
+console.log(countDevelopers(list1))
